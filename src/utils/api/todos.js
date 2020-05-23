fa11f52c5d019dfda656aa126005db247d350893
Romain Tello <romain.tello@gmail.com>
2020-05-23 21:15:42 +0200

    Refactor API handlers

---
+++ src/utils/api/todos.js
@@ -0,0 +1,15 @@
+import crud from './crud';
+
+const todos = crud('todos');
+todos.batchDelete = (todoIds) => {
+  return fetch(`/.netlify/functions/todos-delete-batch`, {
+    body: JSON.stringify({
+      ids: todoIds
+    }),
+    method: 'POST'
+  }).then(response => {
+    return response.json()
+  })
+};
+
+export default todos;
