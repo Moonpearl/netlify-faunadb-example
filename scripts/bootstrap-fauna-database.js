1870b4df0d5aad5a7e649b92bb9123e539e50926
Romain Tello <romain.tello@gmail.com>
2020-05-23 22:26:45 +0200

    Refactor FaunaDB bootstrap function

--- scripts/bootstrap-fauna-database.js
+++ scripts/bootstrap-fauna-database.js
@@ -16,37 +16,68 @@ if (!process.env.FAUNADB_SERVER_SECRET) {
   }
 }
 
-// Has var. Do the thing
-if (process.env.FAUNADB_SERVER_SECRET) {
-  createFaunaDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
-    console.log('Fauna Database schema has been created')
-    console.log('Claim your fauna database with "netlify addons:auth fauna"')
-  })
-}
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+})
 
 /* idempotent operation */
-function createFaunaDB(key) {
-  console.log('Create the fauna database schema!')
-  const client = new faunadb.Client({
-    secret: key
-  })
-
-  /* Based on your requirements, change the schema here */
-  return client.query(q.Create(q.Ref('classes'), { name: 'todos' }))
+const createFaunaDB = new Promise( async (resolve, reject) => {
+  const createClass = (name) =>
+    client.query(q.Create(q.Ref('classes'), { name }))
     .then(() => {
+      console.log(`Created Fauna class '${name}'`)
       return client.query(
         q.Create(q.Ref('indexes'), {
-          name: 'all_todos',
-          source: q.Ref('classes/todos')
+          name: `all_${name}`,
+          source: q.Ref(`classes/${name}`)
         }))
+        .then(() => console.log(`Created index 'all_${name}' in Fauna class '${name}'`))
+        .catch(error => console.error(error))
     }).catch((e) => {
       // Database already exists
-      if (e.requestResult.statusCode === 400 && e.message === 'instance not unique') {
-        console.log('Fauna already setup! Good to go')
-        console.log('Claim your fauna database with "netlify addons:auth fauna"')
+      if (e.requestResult.statusCode === 400 && e.message === 'instance already exists') {
+        console.log(`Fauna class '${name}' already exists`)
+      } else if (e.requestResult.statusCode === 400 && e.message === 'validation failed') {
+        console.log(`Fauna class '${name}' still cached. Please try again in 60 seconds`)
+      } else {
         throw e
       }
     })
+  ;
+
+  console.log('Creating the Fauna database schema...')
+  const classNames = ['todos'];
+
+  try {
+    for (const className of classNames) {
+      await createClass(className);
+    }
+
+    await client.query(
+      q.CreateIndex({
+        name: 'completed_todos',
+        source: q.Collection('todos'),
+        terms: [
+          { field: ['data', 'completed'] },
+        ],
+      })
+    )
+    .then(() => console.log(`Created selection index for class 'todos'`))
+
+    resolve();
+  }
+  catch(error) {
+    reject(error);
+  }
+});
+
+// Has var. Do the thing
+if (process.env.FAUNADB_SERVER_SECRET) {
+  createFaunaDB.then(() => {
+    console.log('Fauna Database schema has been created')
+    console.log('Claim your fauna database with "netlify addons:auth fauna"')
+  })
+  .catch(error => console.error(error))
 }
 
 /* util methods */
