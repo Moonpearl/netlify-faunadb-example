a9a2a701a9c05a340dc6dbc1ad43ab8c7759973a
Romain Tello <44443864+Moonpearl@users.noreply.github.com>
2020-05-24 00:37:56 +0200

    Add get completed todos handlers
    * Add get completed todos handlers

--- src/utils/api/todos.js
+++ src/utils/api/todos.js
@@ -1,15 +1,21 @@
 import crud from './crud';
 
-const todos = crud('todos');
-todos.batchDelete = (todoIds) => {
-  return fetch(`/.netlify/functions/todos-delete-batch`, {
-    body: JSON.stringify({
-      ids: todoIds
-    }),
-    method: 'POST'
-  }).then(response => {
-    return response.json()
-  })
-};
+export default {
+  ...crud('todos'),
 
-export default todos;
+  batchDelete: (todoIds) => {
+    return fetch(`/.netlify/functions/todos-delete-batch`, {
+      body: JSON.stringify({
+        ids: todoIds
+      }),
+      method: 'POST'
+    }).then(response => {
+      return response.json()
+    })
+  },
+
+  getCompleted: () => {
+    return fetch(`/.netlify/functions/todos-completed`)
+    .then(response => response.json());
+  },
+};
